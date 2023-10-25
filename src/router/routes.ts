90ed86bcc8e3e195d77ba2407fc76a0a2593ba51
Kristich Juan Ignacio <j.kristich@etec.local>
2023-10-25 13:02:16 -0300

    tarea prod

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,105 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+
+
+
+interface productoYmercancia{
+
+  nombre: string;
+  precios: number;
+  modelo: string;
+  paisOrigen: string;
+}
+  
+
+
+let productos: productoYmercancia[]  =[
+ {
+   nombre:"",
+   modelo:"",
+   precios: 1500 ,
+   paisOrigen:""
+  
+
+},
+{
+  nombre:"",
+  modelo:"",
+  precios:6,
+  paisOrigen:""
+ 
+
+},
+{
+  nombre:"",
+  modelo:"",
+  precios:6,
+  paisOrigen:""
+ 
+
+},
+{
+  nombre:"",
+  modelo:"",
+  precios:6,
+  paisOrigen:""
+ 
+
+}
+
+]
 
 const router = express.Router();
 
-router.get('/', (_: Request, res: Response) => {
-  res.send('¡Hola Mundo 123451235!');
+router.get('/productos', (_, res) => {
+  res.json(productos);
+  console.log(productos)
 });
+
+
+router.get('/productos/precios/:precios', (req, res) => {
+  const { precios } = req.params;
+  const producto_filtrado = productos.filter((producto) => producto.precios > Number(precios));
+  res.json(producto_filtrado);
+});
+
+
+router.put('/productos/:modelo', (req, res) => {
+  const { modelo } = req.params;
+  const { nombre, precios, paisOrigen } = req.body;
+  const index = productos.findIndex((producto) => producto.modelo === modelo);
+
+  if (index !== -1) {
+    productos[index] = { nombre, precios, modelo, paisOrigen };
+    res.json(productos[index]);
+  } 
+    
+  else {
+    res.status(404).json({ message: 'Producto no encontrado' });
+  }
+});
+// router.get('/productos/precio/:precio', (req, res) => {
+//   const { precio } = req.params;
+//   const filteredProductos = productos.filter((producto) => producto.precio === Number(precio));
+//   res.json(filteredProductos);
+// });
+
+// router.get('/productos/pais/:paisOrigen', (req, res) => {
+//   const { paisOrigen } = req.params;
+//   const filteredProductos = productos.filter((producto) => producto.paisOrigen === paisOrigen);
+//   res.json(filteredProductos);
+// });
+
+// router.post('/productos', (req, res) => {
+//   const { nombre, precio, modelo, paisOrigen } = req.body;
+  
+//   if (!nombre || !precio || !modelo || !paisOrigen) {
+//     return res.status(400).json({ message: 'Faltan campos obligatorios' });
+//   }
+
+//   productos.push({ nombre, precio, modelo, paisOrigen });
+  
+//   return res.json(productos[productos.length -1]);
+// });
 
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 // Importar el módulo Express
 import express from "express";
-
+import router from "./router/routes";
 // Crear una nueva aplicación Express
 const app = express();
 const port = 8081
 // Definir una ruta para el servidor
-
+app.use("/", router)
 
 // Iniciar el servidor en el puerto 3000
 app.listen(port, () => {
